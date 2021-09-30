bfbde97f8b317381c94d00ae0b1665fc6e6f0f08
Zeeshantm <zeeshantm@outlook.com>
2021-09-30 11:35:49 +0530

    Initial Commit

---
+++ API.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+import re
+
+app = Flask(__name__)
+# Set URL & Create Methods for Json Requests 
+@app.route('/v1/sanitized/input', methods = ['GET', 'POST'])
+def home():
+    # Json Handler for GET Method
+    if(request.method=='GET'):
+        data  = "Get Method Working"
+        return jsonify ({'Status':data})
+
+    # Json Handler for POST Method
+    elif(request.method=='POST'):
+        req = request.json['payload']
+
+        # Set SQL Injection Characters
+        exp = re.compile('[!%&*()<>"/\\\|\':]')
+        
+        # Search for vulnerable characters  
+        if(exp.search(req) == None):
+            result = "Sanitized"
+        else:
+            result = "UnSanitized"
+
+        # Return the Output
+        return jsonify({'Result':result})
+
+    app.run()
