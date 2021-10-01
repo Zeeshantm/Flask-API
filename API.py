a00fd4920c7dc21970c7ad31c6a85ef0678bbf3f
Zeeshantm <zeeshantm@outlook.com>
2021-10-01 12:55:21 +0530

    Exeption Handling and Json files Included

--- API.py
+++ API.py
@@ -1,3 +1,4 @@
+import sys
 from flask import Flask, jsonify, request
 import re
 
@@ -5,25 +6,32 @@ app = Flask(__name__)
 # Set URL & Create Methods for Json Requests 
 @app.route('/v1/sanitized/input', methods = ['GET', 'POST'])
 def home():
-    # Json Handler for GET Method
-    if(request.method=='GET'):
-        data  = "Get Method Working"
-        return jsonify ({'Status':data})
+    try:
+    
+        # Json Handler for GET Method
+        if(request.method=='GET'):
+            data  = "Get Method Working"
+            return jsonify ({'Status':data})
 
-    # Json Handler for POST Method
-    elif(request.method=='POST'):
-        req = request.json['payload']
+        # Json Handler for POST Method
+        elif(request.method=='POST'):
+            req = request.json['payload']
 
-        # Set SQL Injection Characters
-        exp = re.compile('[!%&*()<>"/\\\|\':]')
+            # Set SQL Injection Characters
+            exp = re.compile('[!%&*()<>"/\\\|\':]')
         
-        # Search for vulnerable characters  
-        if(exp.search(req) == None):
-            result = "Sanitized"
-        else:
-            result = "UnSanitized"
+            # Search for vulnerable characters  
+            if(exp.search(req) == None):
+                result = "Sanitized"
+            else:
+                result = "UnSanitized"
 
-        # Return the Output
-        return jsonify({'Result':result})
+            # Return the Output
+            return jsonify({'Result':result})
+
+    except Exception as err:
+        return jsonify ({'Error':"Execption in Handler "+err.__str__()})
+        
+
 
     app.run()
